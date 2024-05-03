b4046dad666e9562bd1c556599312413ebcf17e8
Matheus Teixieira <159195517+Math0707@users.noreply.github.com>
2024-05-03 19:21:34 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,31 @@
+text0='VAMOS COMEÇAR COM SOMA'
+
+n1= float(input('Digite um número'))
+n2= float(input('Digite um número'))
+
+soma= n1+n2
+print('Resultado da soma:', soma)
+
+text='AGORA VAMOS PARA A SUBTRAÇÃO'
+print(text)
+n3= float(input('Digite um número'))
+n4= float(input('Digite um número'))
+
+sub= n3-n4
+print('Resultado da subtração:', sub)
+
+text1='AGORA VAMOS PARA A MULTIPLICAÇÃO'
+print(text1)
+n5= float(input('Digite um número'))
+n6= float(input('Digite um número'))
+
+multp= n5*n6
+print('Resultado da multiplicação:', multp)
+
+text1='AGORA VAMOS PARA A DIVISÃO'
+print(text1)
+n7= float(input('Digite um número'))
+n8= float(input('Digite um número'))
+
+div= n7/n8
+print('Resultado da divisão:', div)
